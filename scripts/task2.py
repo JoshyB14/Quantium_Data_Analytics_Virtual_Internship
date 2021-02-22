d17f0e76979cacaa33e7f6749c5bd018aa631e25
JoshyB14 <jbryden14@gmail.com>
2021-02-23 10:50:43 +1100

    fixed Keyerror with corr function

--- scripts/task2.py
+++ scripts/task2.py
@@ -90,32 +90,29 @@ def storeCorr(metric_columns, trial_store_num, stores_12_months=stores_12_months
                                 control stores]
         """
 
-# Extract control stores by taking inverse of the df where trial stores are (by store num)
-        control_store_num = (stores_12_months[~stores_12_months['STORE_NBR'].isin([77,86.88])]
-                        ['STORE_NBR'].unique())
-# Create df with desired column names to store correlation
+        # Extract control stores by taking inverse of the df where trial stores are (by store num)
+        control_store_num = (stores_12_months[~stores_12_months['STORE_NBR'].isin([77,86.88])]['STORE_NBR'].unique())
+        # Create df with desired column names to store correlation
         correlation = pd.DataFrame(columns=['YEAR_MONTH', 'TRIAL_STORE',
                                          'CONTROL_STORE', 'CORR'])
-# Extract the trial stores from the input table
-        trial_stores = stores_12_months[stores_12_months['STORE_NBR']==
-                                 trial_store_num][metric_columns].reset_index()
-# Loop over the control stores
+        # Extract the trial stores from the input table
+        trial_stores = stores_12_months[stores_12_months['STORE_NBR']==trial_store_num][metric_columns].reset_index()
+        # Loop over the control stores
         for store in control_store_num:
-                df = pd.DataFrame(columns= ['YEAR_MONTH', 'TRIAL_STORE', 'CONTROL_STORE'
-                                    'CORR'])
-# For each control store num extract all rows for that particular store
-        control_store = stores_12_months[stores_12_months['STORE_NBR']== store][metric_columns].reset_index()
-# Assign CORR column to the correlation with each trial store to new df
-# (a row for each month and hence correlation for that month)
-        df['CORR'] = trial_stores.corrwith(control_store, axis=1)
-# Assign the trial store num to new df
-        df['TRIAL_STORE'] = trial_store_num
-# Assign the control store num to the df (a row for each month and hence correlation for that month)
-        df['CONTROL_STORE'] = store
-# Assign the year and month for the correlation based off the current input table row
-        df['YEAR_MONTH'] = list(stores_12_months[stores_12_months['STORE_NBR']== trial_store_num]['YEAR_MONTH'])
-# Combine the new df to our master correlation df 
-        correlation = pd.concat([correlation, df])
+                df = pd.DataFrame(columns= ['YEAR_MONTH', 'TRIAL_STORE', 'CONTROL_STORE','CORR'])
+                # For each control store num extract all rows for that particular store
+                control_store = stores_12_months[stores_12_months['STORE_NBR']== store][metric_columns].reset_index()
+                # Assign CORR column to the correlation with each trial store to new df
+                # (a row for each month and hence correlation for that month)
+                df['CORR'] = trial_stores.corrwith(control_store, axis=1)
+                # Assign the trial store num to new df
+                df['TRIAL_STORE'] = trial_store_num
+                # Assign the control store num to the df (a row for each month and hence correlation for that month)
+                df['CONTROL_STORE'] = store
+                # Assign the year and month for the correlation based off the current input table row
+                df['YEAR_MONTH'] = list(stores_12_months[stores_12_months['STORE_NBR']== trial_store_num]['YEAR_MONTH'])
+                # Combine the new df to our master correlation df 
+                correlation = pd.concat([correlation, df])
         return correlation
 
 # %%
@@ -145,35 +142,35 @@ def store_magnitude (metric_columns, trial_store_num, stores_12_months=stores_12
             [magnitude]: [df of the magnitude between the trial store and
                                 control stores]
         """
-# Extract control stores by taking inverse of the df where trial stores are (by store num)
+        # Extract control stores by taking inverse of the df where trial stores are (by store num)
         control_store_num = (stores_12_months[~stores_12_months['STORE_NBR']
                                 .isin([77,86,88])]['STORE_NBR'].unique())
-# Initialize empty df
+        # Initialize empty df
         magnitude = pd.DataFrame()
-# Extract the trial stores from the input table
+        # Extract the trial stores from the input table
         trial_stores = stores_12_months[stores_12_months['STORE_NBR']== trial_store_num][metric_columns]
-# Loop over the control stores
+        # Loop over the control stores
         for store in control_store_num:
-# Take absolute value of: (trial store metric columns of interest minus (-) the control store metric columns 
+        # Take absolute value of: (trial store metric columns of interest minus (-) the control store metric columns 
                 df = abs(stores_12_months[stores_12_months['STORE_NBR']== trial_store_num].reset_index()[metric_columns]
                         - stores_12_months[stores_12_months['STORE_NBR']==store].reset_index()[metric_columns])
-# Assign the trial store num to new df
+        # Assign the trial store num to new df
                 df['TRIAL_STORE'] = trial_store_num
-# Assign the control store num to the df (a row for each month and hence magnitude for that month)
+        # Assign the control store num to the df (a row for each month and hence magnitude for that month)
                 df['CONTROL_STORE'] = store
-# Assign the year and month for the magnitude based off the current input table row
+        # Assign the year and month for the magnitude based off the current input table row
                 df['YEAR_MONTH'] = list(stores_12_months[stores_12_months['STORE_NBR']== trial_store_num]['YEAR_MONTH'])
-# Concat the two df together
+        # Concat the two df together
                 magnitude = pd.concat([magnitude, df])
-# Loop over each column of interest
+        # Loop over each column of interest
         for column in metric_columns:
-# Compute the magnitude:
-# 1- (Observed distance – minimum distance)/(Maximum distance – minimum distance)
+        # Compute the magnitude:
+        # 1- (Observed distance – minimum distance)/(Maximum distance – minimum distance)
                 magnitude[column] = 1 - ((magnitude[column] - magnitude[column].min()) /
                                         (magnitude[column].max() - magnitude[column].min()))
-# Assign the mean value of the metric_columns for each store to a new column called magnitude
+        # Assign the mean value of the metric_columns for each store to a new column called magnitude
         magnitude['MAGNITUDE'] = magnitude[metric_columns].mean(axis=1)
-# Return the df
+        # Return the df
         return magnitude
 
 # %%
@@ -207,8 +204,9 @@ def combine_mag_corr(metric_columns, trial_store_num, stores_12_months=stores_12
         correlation = storeCorr(metric_columns, trial_store_num, stores_12_months)
         # Compute magnitude with store_magnitude function
         magnitude = store_magnitude(metric_columns, trial_store_num, stores_12_months)
-        # Drop 
+        # Drop all rows except magnitude before combining 
         magnitude = magnitude.drop(metric_columns, axis=1)
+        #
         master = pd.merge(correlation, magnitude, on=['CONTROL_STORE', 'TRIAL_STORE', 'YEAR_MONTH'])
         return master
 
@@ -228,7 +226,9 @@ sales_compare = master_sales.groupby(['TRIAL_STORE', 'CONTROL_STORE']).mean().re
 sales_compare['master_score'] = (0.5*sales_compare['CORR']) + (0.5*sales_compare['MAGNITUDE'])
 # For each trial store print the top store with the highest magnitude for total sales
 for trial_store in [77, 86, 88]:
-        print(sales_compare[sales_compare['TRIAL_STORE']==trial_store].sort_values(ascending=False, by='master_score'))
+        # Prints stores with highest master_score based off TOT_SALES alone
+        print(sales_compare[sales_compare['TRIAL_STORE']==trial_store].sort_values(
+                                                                ascending=False, by='master_score').head())
 
 # %%
 # Compare stores based off number of customers (loyalty card info)
@@ -239,24 +239,45 @@ for trial_store in [77, 86, 88]:
         master_customers = pd.concat([master_customers, combine_mag_corr(['LYLTY_CARD_NBR'], trial_store)])
 
 # %%
+# Group customers by trial and control stores and take mean
 customer_compare = master_customers.groupby(['TRIAL_STORE', 'CONTROL_STORE']).mean().reset_index()
 # Combine correlation and magnitude to obtain a new scoring method (0.5 weight for magnitude and 0.5 for correlation )
 # This effectively takes each measure into equal consideration for analysis
 customer_compare['master_score'] = (0.5*customer_compare['CORR']) + (0.5*customer_compare['MAGNITUDE'])
 # For each trial store print the top store with the highest magnitude for total sales
 for trial_store in [77, 86, 88]:
-        print(customer_compare[customer_compare['TRIAL_STORE']==trial_store].sort_values(ascending=False, by='master_score'))
+        # Prints stores with highest master_score based off LYLTY_CARD_NBR alone (num of customers)
+        print(customer_compare[customer_compare['TRIAL_STORE']==trial_store].sort_values(
+                                                        ascending=False, by='master_score').head())
 
 # %%
+# Combine master scores for TOT_SALES and LYLTY_CARD_NBR to obtain control/ trial store pairs
 # Loop over each trial store
 for trial_store in [77, 86, 88]:
         # Set x to be where our sales match our trial stores the best by master score
-        x = (sales_compare[sales_compare['TRIAL_STORE']== trial_store].sort_values(ascending=False, by='master_score')
+        sales = (sales_compare[sales_compare['TRIAL_STORE']== trial_store].sort_values(ascending=False, by='master_score')
                 .set_index(['TRIAL_STORE', 'CONTROL_STORE'])['master_score'])
 
         # Set y to be where our customer numbers match our trial stores the most by master score
-        y = (customer_compare[customer_compare['TRIAL_STORE']== trial_store].sort_values(ascending=False,
+        customers = (customer_compare[customer_compare['TRIAL_STORE']== trial_store].sort_values(ascending=False,
                  by='master_score').set_index(['TRIAL_STORE', 'CONTROL_STORE'])['master_score'])
+        # Print and combine sales/customers taking the average of the master scores and print top 3 pairs (control/trial)
+        print((pd.concat([sales,customers],axis=1).sum(axis=1)/2).sort_values(ascending=False).head(3))
+# Prints out:
+# TRIAL_STORE  CONTROL_STORE
+# 77           233              0.984409
+#              185              0.982973
+#              188              0.982248
+# dtype: float64
+# TRIAL_STORE  CONTROL_STORE
+# 86           155              0.977575
+#              229              0.970710
+#              247              0.969360
+# dtype: float64
+# TRIAL_STORE  CONTROL_STORE
+# 88           40               0.963372
+#              26               0.952071
+#              58               0.950327
+# dtype: float64
 
-        print((pd.concat([x,y],axis=1).sum(axis=1)/2).sort_values(ascending=False).head(3))
-
+# %%
